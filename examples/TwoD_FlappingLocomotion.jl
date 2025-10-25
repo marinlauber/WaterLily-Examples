e56aa34e856301fe885167469275281ef44cd030
marinlauber <marinlauber@tudelft.nl>
2025-10-25 10:57:28 +0200

    update Flapping locomotion example

--- examples/TwoD_FlappingLocomotion.jl
+++ examples/TwoD_FlappingLocomotion.jl
@@ -1,23 +1,23 @@
-using WaterLily,Plots,StaticArrays
+using WaterLily,Plots,StaticArrays,BiotSavartBCs
 
-# # Biot-Savart momentum step with U and acceleration prescribed
-# import BiotSavartBCs: biot_mom_step!,biot_project!
-# function biot_mom_step!(a::Flow{N},b,ω...;λ=quick,udf=nothing,fmm=true,U,kwargs...) where N
-#     a.u⁰ .= a.u; scale_u!(a,0); t₁ = sum(a.Δt); t₀ = t₁-a.Δt[end]
-#     # predictor u → u'
-#     @log "p"
-#     conv_diff!(a.f,a.u⁰,a.σ,λ,ν=a.ν)
-#     udf!(a,udf,t₀; kwargs...)
-#     BDIM!(a);
-#     biot_project!(a,b,ω...,U;fmm) # new
-#     # corrector u → u¹
-#     @log "c"
-#     conv_diff!(a.f,a.u,a.σ,λ,ν=a.ν)
-#     udf!(a,udf,t₁; kwargs...)
-#     BDIM!(a); scale_u!(a,0.5)
-#     biot_project!(a,b,ω...,U;fmm,w=0.5) # new
-#     push!(a.Δt,CFL(a))
-# end
+# Biot-Savart momentum step with U and acceleration prescribed
+import BiotSavartBCs: biot_mom_step!,biot_project!
+function biot_mom_step!(a::Flow{N},b,ω...;λ=quick,udf=nothing,fmm=true,U,kwargs...) where N
+    a.u⁰ .= a.u; scale_u!(a,0); t₁ = sum(a.Δt); t₀ = t₁-a.Δt[end]
+    # predictor u → u'
+    @log "p"
+    conv_diff!(a.f,a.u⁰,a.σ,λ,ν=a.ν)
+    udf!(a,udf,t₀; kwargs...)
+    BDIM!(a);
+    biot_project!(a,b,ω...,U;fmm) # new
+    # corrector u → u¹
+    @log "c"
+    conv_diff!(a.f,a.u,a.σ,λ,ν=a.ν)
+    udf!(a,udf,t₁; kwargs...)
+    BDIM!(a); scale_u!(a,0.5)
+    biot_project!(a,b,ω...,U;fmm,w=0.5) # new
+    push!(a.Δt,CFL(a))
+end
 
 #  momentum step with U and acceleration prescribed
 import WaterLily: mom_step!,scale_u!,conv_diff!,udf!,accelerate!,BDIM!,CFL,project!
@@ -56,9 +56,9 @@ WaterLily.CFL(a::Flow) = WaterLily.CFL(a;Δt_max=0.1)
 function plate(L=2^6;ν,A=1/2,U=1,ϵ=0.5,Λ=4,f=0.5f0,thk=2ϵ+√2,mem=Array)
     # pure heave motion
     function map(x,t)
-        return x - SA[3L,3L-0.5f0*A*L*sin(2π*f*t*U/L)]
+        return x - SA[4L,4L-0.5f0*A*L*sin(2π*f*t*U/L)]
     end
-    Simulation((6L,6L),(0,0),L;U,ν,body=AutoBody((x,t)->ellipse(x,t;radius=L/2,Λ=Λ),map),ϵ,mem)
+    BiotSimulation((8L,8L),(0,0),L;U,ν,body=AutoBody((x,t)->ellipse(x,t;radius=L/2,Λ=Λ),map),ϵ,mem)
 end
 
 
@@ -71,7 +71,7 @@ function run(L=64,U=1,Λ=4.f0,radius=L/2.f0)
     # Ref = Laf/ν       -> ν = Laf/Ref = AL²f/Ref
     M = 1.f0
     A = 1.f0
-    Ref = 10^4.f0
+    Ref = 10^2.f0
     f = 1.f0
 
     # derived parameters
@@ -86,16 +86,16 @@ function run(L=64,U=1,Λ=4.f0,radius=L/2.f0)
     sim = plate(L;A,ν=A*L^2*f/Ref,mem=Array)
     store=[]
 
-    @gif for tᵢ in range(0,10.;step=0.05)
+    @gif for tᵢ in range(0,50.;step=0.05)
         # update
         t = sum(sim.flow.Δt[1:end-1])
         while t < tᵢ*sim.L/sim.U
             Δt = sim.flow.Δt[end]
             # remeasure the sim
             measure!(sim)
-            # biot_mom_step!(sim.flow,sim.pois,sim.ω,sim.x₀,sim.tar,sim.ftar;
-                        #    fmm=sim.fmm,udf=fall!,acceleration=-a0,U=-vel) # change of frame
-            mom_step!(sim.flow,sim.pois;udf=fall!,acceleration=-a0,U=-vel)
+            biot_mom_step!(sim.flow,sim.pois,sim.ω,sim.x₀,sim.tar,sim.ftar;
+                           fmm=sim.fmm,udf=fall!,acceleration=-a0,U=-vel) # change of frame
+            # mom_step!(sim.flow,sim.pois;udf=fall!,acceleration=-a0,U=-vel)
 
             # compute pressure force and moment
             force = -WaterLily.total_force(sim) # in lab frame, fix y
@@ -107,19 +107,18 @@ function run(L=64,U=1,Λ=4.f0,radius=L/2.f0)
             a0 = copy(accel)
 
             # save position, velocity, etc
-            push!(store,[Δt,force...,accel...,pos...,vel...])
+            push!(store,[Δt,force...,accel...,pos[1],0.5f0sim.L*sin(2π*t*sim.U/sim.L),vel...])
 
-            # update time, must be done globally to set the pos/vel correctly
-            t_init = t; t += Δt
+            # update time
+            t += Δt
         end
         # plot vorticity
         @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
         @inside sim.flow.σ[I] = ifelse(sdf(sim.body,loc(0,I),WaterLily.time(sim))<0,NaN,sim.flow.σ[I])
         flood(sim.flow.σ|>Array,shift=(-1.5,-1.5),clims=(-5,5), axis=([], false),
               cfill=:seismic,legend=false,border=:none,size=(1080,1080))
-        body_plot!(sim);
         xs = SA[3sim.L,3sim.L] - sim.body.map(SA[3sim.L,3sim.L],WaterLily.time(sim))
-        quiver!([xs[1]],[xs[2]],quiver=([force[1]],[force[2]]),color=:black,lw=2,arrow=:closed,legend=false)
+        quiver!([xs[1]],[xs[2]],quiver=([force[1]/10],[force[2]/10]),color=:black,lw=2,arrow=:closed,legend=false)
         quiver!([xs[1]],[xs[2]],quiver=([sim.L*vel[1]],[sim.L*vel[2]]),color=:red,lw=2,arrow=:closed,legend=false)
         # print time step
         println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3),
