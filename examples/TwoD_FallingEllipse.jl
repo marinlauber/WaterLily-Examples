ea1b084633621053c0ad527c454a047d8e9e777e
marinlauber <marinlauber@gmail.com>
2025-12-09 08:32:02 +0100

    add falling object sims

--- examples/TwoD_FallingEllipse.jl
+++ examples/TwoD_FallingEllipse.jl
@@ -72,6 +72,26 @@ fall!(flow,t;acceleration) = for i ∈ 1:ndims(flow.p)
     @loop flow.f[I,i] += acceleration[i] over I ∈ CartesianIndices(flow.p)
 end
 
+# equilibrium in body frame of reference
+function equilibrium!(du,u,p,t)
+    # states
+    _,_,vₓ,vᵧ,aₓ,aᵧ,_,ω,dω,fₓ,fᵧ,m = u
+    # parameters
+    m,mₐ,g,Im,Iₐ = p
+    # rates
+    du[1:2] .= SA[vₓ,vᵧ]
+    # here we need to fill the solution for the acceleration, and the rate for the velocity
+    u[5:6] = du[3:4] = (SA[fₓ,fᵧ] + m.*g - mₐ.*SA[aₓ,aᵧ])./(m .+ mₐ)
+    du[7] = ω
+    u[9] = du[8] = (m - ω*Iₐ)/(Im+Iₐ)
+end
+# set new initial condition and solve a step dt of the ODE problem
+function solve_ODE!(problem,u¹,dt)
+    SciMLBase.set_u!(problem,u¹)
+    OrdinaryDiffEq.step!(problem,dt,true)
+    return problem.u
+end
+
 #helper to rotate a vector
 @inline @fastmath rotate(v,θ) = SA[cos(θ) -sin(θ); sin(θ) cos(θ)]*v
 # needed for stability of the ODE solver
@@ -83,57 +103,64 @@ function run(L=128,U=1,Λ=4.f0,radius=L/2.f0)
     ρ = 10.f0 # buoyancy corrected density
     mₐ = SA[π*radius^2,π*radius^2/Λ^2] # added-mass coefficient ellipse
     m = ρ*π*radius^2/Λ # mass
-    vel = SA[0.f0,0.f0]
-    a0 = SA[0.f0,0.f0]
-    pos = SA[0.f0,0.f0]
+    vel = SA[0.f0,0.f0]; acc = SA[0.f0,0.f0]; pos = SA[0.f0,0.f0]
     g = SA[0.f0,-U^2/L]
 
     # rotation variables
     Im = 0.25f0*m*(radius^2+radius^2/Λ^2)
     Iₐ = 0.125f0*π*(radius^2-radius^2/Λ^2)^2 # added mass ellipse m₆₆
-    dω = 0.0f0; dω₀=0.0f0; ω = 0.0f0; rot =0.0f0
-    α = 0.05f0
+    dω = 0.0f0; ω = 0.0f0; α₀ =0.05f0; α = 0.0f0
 
     # make the sim
-    center,force = SA[2.f0L,L],SA[0.f0,0.f0]
-    sim = circle(L;center=center,α0=α,mem=Array)
+    center,forces,moment = SA[2.f0L,L],SA[0.f0,0.f0],0.0f0
+    sim = circle(L;center=center,α0=α₀,mem=Array)
     store=[]
+    # initial conditions _,_,vₓ,vᵧ,aₓ,aᵧ,_,ω,dω,fₓ,fᵧ,m = u[1:12]
+    u₀ = [pos...,vel...,acc...,α,ω,dω,forces...,moment]
+    params = (m,mₐ,g,Im,Iₐ)
+    equilibrium = init(ODEProblem(equilibrium!,u₀,(0,1000),params),Tsit5(),abstol=1e-6,reltol=1e-6,save_everystep=false)
 
     @gif for tᵢ in range(0,40.0;step=0.1)
         # update
-        t = sum(sim.flow.Δt[1:end-1])
-        while t < tᵢ*sim.L/sim.U
+        # t = sum(sim.flow.Δt[1:end-1])
+        while sim_time(sim) < tᵢ
             Δt = sim.flow.Δt[end]
             # remeasure the sim
             measure!(sim)
             biot_mom_step!(sim.flow,sim.pois,sim.ω,sim.x₀,sim.tar,sim.ftar;
-                           fmm=sim.fmm,udf=fall!,acceleration=-a0,U=-vel) # change of frame
+                           fmm=sim.fmm,udf=fall!,acceleration=-acc,U=-vel) # change of frame
 
-            # compute pressure force and moment
-            force = -WaterLily.pressure_force(sim) # in lab frame
+            # compute pressure forces and moment
+            forces = -WaterLily.pressure_force(sim) # in lab frame
             moment = WaterLily.pressure_moment(center,sim)[1]
-            force = rotate(force,α+rot) # transform to body frame
-            a0 = rotate(a0,α+rot)
+            forces = rotate(forces,α₀+α) # transform to body frame
+            acc = rotate(acc,α₀+α)
 
-            # update force in body frame
-            accel = (force + m.*g - mₐ.*a0)./(m .+ mₐ)
-            accel = rotate(accel,-(α+rot)) # back to lab frame
-            pos = pos + Δt.*(vel+Δt.*accel./2.)
-            vel = vel + Δt.*accel
-            a0 = copy(accel)
-            dω = (moment - dω₀*Iₐ)/(Im+Iₐ)
-            rot = rot + Δt.*(ω+Δt*dω/2.)
+            # # update forces in body frame
+            acc = (forces + m.*g - mₐ.*acc)./(m .+ mₐ)
+            acc = rotate(acc,-(α₀+α)) # back to lab frame
+            pos = pos + Δt.*(vel+Δt.*acc./2.)
+            vel = vel + Δt.*acc
+            dω = (moment - dω*Iₐ)/(Im+Iₐ)
+            α =α + Δt.*(ω+Δt*dω/2.)
             ω = ω + Δt*dω
-            dω₀ = copy(dω)
+            # solve the ODE
+            # u=solve_ODE!(equilibrium,[pos...,vel...,acc...,α,ω,dω,forces...,moment],sim.flow.Δt[end])
+            # pos,vel,acc,α,ω,dω = u[1:2],u[3:4],u[5:6],u[7],u[8],u[9]
+            # @show pos,vel,acc,α,ω,dω
+            #rotate back in lab frame
+            acc = rotate(acc,-(α₀+α))
+            vel = rotate(vel,-(α₀+α))
+            pos = rotate(pos,-(α₀+α))
 
             # update the body
-            sim.sim.body = RigidBody((x,t)->ellipse(x,t;radius=L/2,Λ=Λ),center,SA[0.f0,0.f0],α+rot,ω)
+            # sim.sim.body = RigidBody((x,t)->ellipse(x,t;radius=L/2,Λ=Λ),center,SA[0.f0,0.f0],α₀+α,ω)
 
             # save position, velocity, etc
-            push!(store,[Δt,force...,accel...,pos...,vel...,α+rot])
+            push!(store,[Δt,pos...,vel...,acc...,α₀+α,ω,dω,forces...,moment])
 
             # update time, must be done globally to set the pos/vel correctly
-            t_init = t; t += Δt
+            # t_init = t; t += Δt
         end
         # plot vorticity
         @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
@@ -144,7 +171,7 @@ function run(L=128,U=1,Λ=4.f0,radius=L/2.f0)
         plot!([center[1]],[center[2]],marker=:o,color=:red,legend=:none)
         # print time step
         println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3),
-                " F₂=", round(force[2],digits=3), " a₂=", round(a0[2],digits=3))
+                " F₂=", round(forces[2],digits=3), " a₂=", round(acc[2],digits=3))
     end
     return sim,store
 end
@@ -153,10 +180,10 @@ sim,data = run(); # run the sim
 
 let # post-process
     store_matrix = reduce(vcat,data')
-    p1 = plot(store_matrix[:,6],store_matrix[:,7],linez=cumsum(store_matrix[:,1]),
+    p1 = plot(store_matrix[:,2],store_matrix[:,3],linez=cumsum(store_matrix[:,1]),
                 colorbar=:true, aspect_ratio=:equal,label=:none, lw=3, xlabel="X", ylabel="Y",)
-    p2 = plot(cumsum(store_matrix[:,1]),[store_matrix[:,8],store_matrix[:,9]],
+    p2 = plot(cumsum(store_matrix[:,1]),[store_matrix[:,4],store_matrix[:,5]],
                 colorbar=:false, label=["Vx" "Vy"])
-    p3 = plot(cumsum(store_matrix[:,1]),store_matrix[:,10],colorbar=:false, label="rotation")
+    p3 = plot(cumsum(store_matrix[:,1]),store_matrix[:,8],colorbar=:false, label="rotation")
     plot(p1, p2, p3, layout= @layout[a{0.5w} [grid(2,1)]])
 end
